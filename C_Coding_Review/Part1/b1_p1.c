7896eed8d9752a4f421ed9c600ed6741006fd9ea
Trần Đức Hưng <hungpl2310@gmail.com>
2026-01-08 15:27:10 +0700

    Create b1_p1.c

---
+++ C_Coding_Review/Part1/b1_p1.c
@@ -0,0 +1,33 @@
+/*Tính và in ra phần nguyên, phần dư của phép chia 2 số nguyên a, b. 
+Input  : 2 số nguyên a, b với b khác 0( -1018 ≤a, b ≤1018)  
+Output  : Đáp án của bài toán được in trên 1 dòng 
+*/
+
+#include<stdio.h>
+
+void div(long long a , long long b , long long *quotient, long long *remainder)
+{
+	if(b==0)
+	{
+		*quotient = 0;
+        *remainder = 0;
+    	return;
+	}
+	
+    *quotient = a / b;
+    *remainder = a % b;	
+}
+
+int main()
+{
+    long long a, b;
+    long long q, r;
+
+    scanf("%lld %lld", &a, &b);
+
+    div(a, b, &q, &r);
+
+    printf("%lld %lld", q, r);
+
+    return 0;	
+}
