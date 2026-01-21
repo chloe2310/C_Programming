fe52d63c57cf773775ea4cf9ff0f16b7e3ae7ed6
Trần Đức Hưng <hungpl2310@gmail.com>
2026-01-21 10:54:01 +0700

    Create B1_Student_Structure_Operations.c

---
+++ C_Topic/Structure/B1_Student_Structure_Operations.c
@@ -0,0 +1,2 @@
+/* Create a structure called "Student" with members name, age, and total marks. Write a C program to input data for two students, display their information,
+and find the average of total marks. */
