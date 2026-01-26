1822c43f6ceb35ab356d4599d1c6ac28daed4b99
Trần Đức Hưng <hungpl2310@gmail.com>
2026-01-27 02:30:26 +0700

    Update b1.c

--- 250_exercises/b1.c
+++ 250_exercises/b1.c
@@ -1,1 +1,16 @@
-Reup 250 bài tập ôn tập 
+/* Nhập vào diện tích S của một mặt cầu. Tính thể tích V của hình cầu này. */
+
+
+#include <stdio.h> 
+#include <math.h> 
+ 
+int main() { 
+  double S; 
+   
+  printf( "Nhap dien tich S: " ); 
+  scanf( "%lf", &S ); 
+  printf( "The tich V = %g\n", 
+    ( 4 * M_PI * pow( sqrt( S / ( 4 * M_PI ) ), 3 ) ) / 3 ); 
+  return 0; 
+} 
+   
