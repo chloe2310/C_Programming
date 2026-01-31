b47614c3698a3631a7aecc9fc6f0db3a4cdfa70e
Trần Đức Hưng <hungpl2310@gmail.com>
2026-02-01 00:16:29 +0700

    Create b18.c

---
+++ 250_exercises/b18.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+ 
+int main() 
+{ 
+  long h; 
+ 
+  printf( "Nhap so gio: " ); 
+  scanf( "%ld", &h ); 
+  printf( "%ld tuan, %ld ngay, %ld gio\n", 
+       h / ( 24 * 7 ), ( h % ( 24 * 7 ) ) / 24, ( h % ( 24 * 7 ) ) % 24 ); 
+  return 0; 
+} 
