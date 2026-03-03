6c299733a5c48863b3f857878a757404d36fd1fb
hung <hungpl2310@gmail.com>
2026-03-03 21:54:52 +0700

    add file hello_world.c

--- C_Topic/Structure/B1_Student_Structure_Operations.c
+++
@@ -1,2 +0,0 @@
-/* Create a structure called "Student" with members name, age, and total marks. Write a C program to input data for two students, display their information,
-and find the average of total marks. */
---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("hello everyone , I'm Chloe\n");
+	return 0;
+}
